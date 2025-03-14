aa2717c56212bc8fb45f3e38b7ea0f514b15f89c
Youssef Faisal <125581694+youssefx99@users.noreply.github.com>
2025-03-14 18:46:19 +0200

    Delete routes directory

--- routes/streaming.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const streamingController = require("../controllers/streamingController");
-const authMiddleware = require("../controllers/authController");
-
-router.post("/generate-video", streamingController.generateVideo);
-router.post("/extract-frames", streamingController.extractFrames);
-
-module.exports = router;
